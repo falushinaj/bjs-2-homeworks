15ab72a7ec6bb7cd3a93ba088b9058cff9017239
falushinaj <119869603+falushinaj@users.noreply.github.com>
2023-02-15 16:53:28 +0100

    Add files via upload
    Первое решение задания по теме Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,47 @@
-function Student(name, gender, age) {
+function Student(name, gender, age, marks = []) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = marks;
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  let student1 = new Student("Василиса", "женский", 19);
+  let student2 = new Student("Артём", "мужской", 25);
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+      this.marks = undefined;
+    } else {
+      this.marks = this.marks.push(marks);
+    }
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
+  Student.prototype.getAverage = function () {
+    if (this.marks === undefined) {
+      return 0;
+    } else {
+      return this.marks.reduce((a, v) => a + v/this.marks.length, 0)
+    }
+  }
   
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
+  
+  student1.setSubject("Algebra");
+  console.log(student1.getAverage()); // 0
+  student1.addMarks(4, 5, 4, 5);
+  console.log(student1.getAverage()); // 4.5
+  console.log(student1);
+  // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+  
+  student2.setSubject("Geometry");
+  student2.exclude('плохая учёба')
+  console.log(student2)
+  // {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
