1af07f3430946529fdd44d1eb98371107f5d34b8
falushinaj <119869603+falushinaj@users.noreply.github.com>
2023-03-14 17:23:27 +0100

    Add files via upload
    Решение задачи 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,35 +1,97 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-    let cache = [];
-    function wrapper(...args) {
-        const hash = args.join(',');
-        let objectInCache = cache.findIndex((item) => item.hash == hash);
-        if (objectInCache !== -1) {
-          console.log("Из кэша: " + cache[objectInCache].value);
-          return "Из кэша: " + cache[objectInCache].value;
-        }
-     
-        let result = func(...args);
-        cache.push({
-          'hash': hash,
-          'value': result
-        });
-        if (cache.length > 5) {
-          cache.shift();
-        }
-        console.log("Вычисляем: " + result);
-        return "Вычисляем: " + result;
+  let cache = [];
+  function wrapper(...args) {
+      const hash = args.join(',');
+      let objectInCache = cache.findIndex((item) => item.hash == hash);
+      if (objectInCache !== -1) {
+        console.log("Из кэша: " + cache[objectInCache].value);
+        return "Из кэша: " + cache[objectInCache].value;
       }
-      return wrapper;
+   
+      let result = func(...args);
+      cache.push({
+        'hash': hash,
+        'value': result
+      });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 const addAndMultiply = (a, b, c) => (a + b) * c;
 const upgraded = cachingDecoratorNew(addAndMultiply);
-upgraded(1, 2, 3); // вычисляем: 9
-upgraded(1, 2, 3); // из кэша: 9
-upgraded(2, 2, 3); // вычисляем: 12
-upgraded(3, 2, 3); // вычисляем: 15
-upgraded(4, 2, 3); // вычисляем: 18
-upgraded(5, 2, 3); // вычисляем: 21
-upgraded(6, 2, 3); // вычисляем: 18 (при этом кеш для 1, 2, 3 уничтожается)
-upgraded(1, 2, 3); // вычисляем: 9  (снова вычисляем, кеша нет)
+// upgraded(1, 2, 3); // вычисляем: 9
+// upgraded(1, 2, 3); // из кэша: 9
+// upgraded(2, 2, 3); // вычисляем: 12
+// upgraded(3, 2, 3); // вычисляем: 15
+// upgraded(4, 2, 3); // вычисляем: 18
+// upgraded(5, 2, 3); // вычисляем: 21
+// upgraded(6, 2, 3); // вычисляем: 18 (при этом кеш для 1, 2, 3 уничтожается)
+// upgraded(1, 2, 3); // вычисляем: 9  (снова вычисляем, кеша нет)
+
+
+// ЗАДАЧА 2
+
+
+let timeout;
+let firstCall = true;
+
+function debounceDecoratorNew(func, delay) {
+  
+  return function (...args) {
+    if (firstCall) {
+      func.call(this, ...args);
+      firstCall = false;
+      return
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.call(this, ...args);
+      firstCall = true;
+    }, delay);
+    spyDecoratorNew();
+  };  
+}
+
+let count = 0;
+let allCount = 0;
+
+function spyDecoratorNew(func, delay) {
+  function wrapper(...args) {
+    if (firstCall) {
+    wrapper.histori.push(args);
+    firstCall = false;
+    wrapper.count++;
+    console.log(wrapper.count);
+    return func(...args);
+    }
+  }   
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    func.call(this, ...args);
+    firstCall = true;
+  }, delay);
+  wrapper.histori = [];
+  console.log(wrapper.histori);
+  return wrapper;
+};   
+
+const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован, так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
+setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано, так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
+setTimeout(() => upgradedSendSignal(3, 900), 900); // проигнорировано, так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
+setTimeout(() => upgradedSendSignal(4, 1200), 1200); // проигнорировано, так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
+setTimeout(() => upgradedSendSignal(5, 2300), 2300); // Сигнал отправлен, так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => upgradedSendSignal(6, 4400), 4400); // проигнорировано, так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
+setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+setTimeout(() => {
+  console.log(upgradedSendSignal.count); // было выполнено 3 отправки сигнала
+  console.log(upgradedSendSignal.allCount); // было выполнено 6 вызовов декорированной функции
+}, 7000)
+
